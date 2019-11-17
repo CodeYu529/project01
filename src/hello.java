5315e138465b9a394671a5cc79823996a40c5c26
CodeYu529 <516750613@qq.com>
2019-11-17 21:47:35 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,15 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.util.ArrayList;
+
+public class hello {
+
+    public static void main(String[] args){
+        System.out.println("helloword!!!");
+
+        ArrayList list = new Arraylist();
+
+
+
+    }
+}
